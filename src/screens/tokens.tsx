e85686123419bcec09754450e256ec59fc8431b8
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-10-05 21:45:17 +0100

    feat: code celan up doen

--- src/screens/tokens.tsx
+++ src/screens/tokens.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View, StyleSheet, Text } from 'react-native';
+import { ScrollView, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import TopBar from '../components/topbar';
 import AdvertisementBanner from '../components/advert';
 import TabBar from '../components/tapbar';
@@ -11,6 +11,7 @@ import Loader from '../components/loader';
 import { getFavoriteTokens } from '../utils/constants';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../routes';
+import Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const coinApi = new CoinApi();
 
@@ -50,6 +51,50 @@ const Tokens = (props : propsType) => {
     <View style={styles.container}>
       <TopBar />
       <AdvertisementBanner />
+      <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Icon name="wallet" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>Academy</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="family-tree" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>Referral</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="square-rounded-outline" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>Square</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="currency-btc" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>Earn</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="ethereum" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>Pay</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="credit-card-sync" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>P2P</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="gift-outline" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>Giveaway</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Icon name="ellipse-outline" size={20} color="#ffb31a" />
+            <Text style={styles.buttonText}>More</Text>
+            {/* <View style={styles.newBadge}>
+              <Text style={styles.newText}>New</Text>
+            </View> */}
+          </TouchableOpacity>
+        </View>
       <TabBar activeTab={activeTab} onTabPress={setActiveTab} />
       <CryptoListHeader/>
       {activeTab && activeTab === 'Hot' ? (
@@ -119,6 +164,48 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginHorizontal: 'auto',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    backgroundColor: 'white'
+  },
+  button: {
+    width: '25%',
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    position: 'relative',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    elevation: 0,
+  },
+  buttonText: {
+    marginTop: 8,
+    fontSize: 9,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  newBadge: {
+    position: 'absolute',
+    top: 5,
+    right: 10,
+    backgroundColor: '#ffb31a',
+    paddingHorizontal: 3,
+    paddingVertical: 2,
+    borderRadius: 5,
+  },
+  newText: {
+    fontSize: 9,
+    color: '#fff',
   },
 });
 
